961fbf4e395d8024e143bfaeb7a3c638a08eae51
Ahmet Imamoglu <ahmet363@gmail.com>
2021-12-02 16:24:11 +0300

    soz/update_soz_form_add güncellendi.

--- trunk/soz/update_soz_form_add.php
+++ trunk/soz/update_soz_form_add.php
@@ -1,24 +1,35 @@
-<?php if(!defined('AHMETI_KONTROL')){ echo 'Bu dosyaya erşiminiz engellendi.'; exit(); } ?>
 <?php
-// Güncelle ok
 
-$id=mysql_real_escape_string($_POST['id']);
-$soz=mysql_real_escape_string(trim(stripslashes($_POST['soz'])));
-$aciklama=mysql_real_escape_string(trim(stripslashes($_POST['aciklama'])));
-$author_id=(int)$_POST['author_id'];
+if( ! defined('AHMETI_KONTROL') ){ echo 'Bu dosyaya erşiminiz engellendi.'; exit(); }
 
-if(empty($soz) || empty($author_id)){
+if ( empty($_POST) ){
+    return '';
+}
+
+$id = isset($_POST['id']) ? $_POST['id'] : '';
+$quote = isset($_POST['soz']) ? $_POST['soz'] : '';
+$quoteDesc = isset($_POST['aciklama']) ? $_POST['aciklama'] : '';
+$authorId = isset($_POST['author_id']) ? $_POST['author_id'] : '';
+
+
+if( empty($id) || empty($quote) || empty($authorId)){
 
     echo '<p class="ahmeti_hata">Boş alan bırakmayınız.</p>';
 
 }else{
-    $ahmetiPre=AHMETI_WP_PREFIX;
-    $sql=mysql_query("UPDATE {$ahmetiPre}soz SET soz='$soz',aciklama='$aciklama',soz_author_id='$author_id' WHERE soz_id=$id");
 
-    if ($sql){
+    global $wpdb;
+    $status = $wpdb->update(AHMETI_WP_QUOTES_TABLE, [
+        'quote_author_id' => $authorId,
+        'quote' => $quote,
+        'quote_desc' => $quoteDesc,
+    ], [
+        'quote_id' => $id
+    ]);
+
+    if ( $status ){
         echo '<p class="ahmeti_ok">Söz başarıyla güncellendi.</p>';
     }else{
         echo '<p class="ahmeti_hata">Söz güncellenirken hata oluştu.</p>';
     }
 }
-?>

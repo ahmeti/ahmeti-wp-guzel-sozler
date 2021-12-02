841cdac177fbe956391592f82f2e1830f420ea1a
Ahmet Imamoglu <ahmet363@gmail.com>
2021-12-02 17:07:39 +0300

    author/update_author_form güncellendi.

--- trunk/author/update_author_form.php
+++ trunk/author/update_author_form.php
@@ -1,40 +1,20 @@
-<?php if(!defined('AHMETI_KONTROL')){ echo 'Bu dosyaya erşiminiz engellendi.'; exit(); } ?>
+<?php if( ! defined('AHMETI_KONTROL') ){ echo 'Bu dosyaya erşiminiz engellendi.'; exit(); } ?>
+
 <h2>Yazar Düzenle</h2>
+
 <?php
-// Söz Güncelleme Sayfası
-
-$id=(int)$_GET['id'];
-$ahmetiPre=AHMETI_WP_PREFIX;
-$row=mysql_fetch_object(mysql_query("SELECT * FROM {$ahmetiPre}soz_author WHERE wp_soz_author_id=$id"));
-
-
-/* Soru ve şıklar için TinyMce */
-function ShowTinyMCE() {
-    // conditions here
-    wp_enqueue_script( 'common' );
-    wp_enqueue_script( 'jquery-color' );
-    wp_print_scripts('editor');
-    if (function_exists('add_thickbox')) add_thickbox();
-    wp_print_scripts('media-upload');
-    if (function_exists('wp_tiny_mce')) wp_tiny_mce();
-    wp_admin_css();
-    wp_enqueue_script('utils');
-    do_action("admin_print_styles-post-php");
-    do_action('admin_print_styles');
-}
-add_filter('admin_head','ShowTinyMCE');
-
-
+$id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+$author = ahmeti_wp_db()->get_row(ahmeti_wp_db()->prepare('SELECT * FROM '.AHMETI_WP_AUTHORS_TABLE.' WHERE author_id = %d', [$id]));
 ?>
 
 <div style="display: block;padding: 0 0 10px 0;width: 700px">
     <form id="form_gonder" action="<?php echo PHP_D_URL; ?>&islem=update_author_form_add" method="post">
         <h3 style="margin-bottom: 1px;">Yazar Adı</h3>
-        <input type="text" name="kisi" size="40" value="<?php echo $row->wp_soz_author_name; ?>"/>
+        <input type="text" name="kisi" size="40" value="<?php echo $author->author_name; ?>"/>
         <br/><br/>
-        <?php wp_editor($row->author_content,'author_content',$settings = array('textarea_rows'=> 15 ,'wpautop' => false));?>
+        <?php wp_editor($author->author_content, 'author_content', ['textarea_rows'=> 15 ,'wpautop' => false]); ?>
         <br/><br/>
         <input type="submit" value="Yazarı Güncelle" class="button" id="gonder_button"/>
-        <input type="hidden" name="id" value="<?php echo $row->wp_soz_author_id; ?>" />
+        <input type="hidden" name="id" value="<?php echo $id; ?>" />
     </form>
 </div>

1017efb11e7e2c1307653d754aba99bc75075526
Ahmet Imamoglu <ahmet363@gmail.com>
2021-12-02 17:07:02 +0300

    author/author_list güncellendi.

--- trunk/author/author_list.php
+++ trunk/author/author_list.php
@@ -2,63 +2,42 @@
 <h2>Yazar Listesi</h2>
 <?php
 
-/* Sayfalama İçin */
-$page=@$_GET['is_page'];
-$page_limit=AHMETI_SOZ_LIMIT;
-$ahmetiPre=AHMETI_WP_PREFIX;
-$soz_listesi_row=mysql_fetch_assoc(mysql_query("SELECT COUNT(wp_soz_author_id) FROM {$ahmetiPre}soz_author"));
+$count = (ahmeti_wp_db()->get_row(ahmeti_wp_db()->prepare('SELECT COUNT(author_id) as count FROM '.AHMETI_WP_AUTHORS_TABLE, [])))->count;
+$page = isset($_GET['is_page']) && (int)$_GET['is_page'] > 0 ? (int)$_GET['is_page'] : 1;
+$start = ($page - 1) * AHMETI_SOZ_LIMIT;
 
-if(empty($page) || !is_numeric($page)){
-    $baslangic=1;
-    $page=1;
-}else{
-    $baslangic=$page;
-}
-$toplam_sayfa=$soz_listesi_row['COUNT(wp_soz_author_id)'];
-$baslangic=($baslangic-1)*$page_limit;
-$siralama=AHMETI_SIRALAMA;
+$items = ahmeti_wp_guzel_sozler_authors(
+	['author_id', 'author_name', 'author_slug', 'author_content'],
+	['author_id', 'DESC'],
+	[$start, AHMETI_SOZ_LIMIT]
+);
 
-$soz_listesi=mysql_query("SELECT wp_soz_author_id,wp_soz_author_name FROM {$ahmetiPre}soz_author ORDER BY wp_soz_author_name ASC LIMIT $baslangic,$page_limit");
-
-
-if($toplam_sayfa > 0){
+if( count($items) > 0 ){
     ?>
     <table class="admin_soz_table">
         <tr class="tr_baslik">
-            <td style="width: 50px;">ID</td>
-            <td style="width: 250px;">Yazar</td>
-            <td style="width: 80px;">Düzenle</td>
-            <td style="width: 80px;">Sil</td>
+            <td style="white-space: nowrap">ID</td>
+            <td style="white-space: nowrap">Düzenle</td>
+            <td style="white-space: nowrap">Sil</td>
+            <td>Yazar</td>
         </tr>
         <?php
-        while ($soz=mysql_fetch_object($soz_listesi)){
-        ?>
+        foreach ($items as $item){ ?>
         <tr>
-            <td><?php echo $soz->wp_soz_author_id; ?></td>
-            <td><?php echo $soz->wp_soz_author_name; ?></td>
+            <td><?php echo $item->author_id; ?></td>
             <td>
-                <a href="<?php echo PHP_D_URL; ?>&islem=author_guncelle&id=<?php echo $soz->wp_soz_author_id; ?>">Düzenle</a>
+                <a href="<?php echo PHP_D_URL; ?>&islem=author_guncelle&id=<?php echo $item->author_id; ?>">Düzenle</a>
             </td>
             <td>
-                <a onclick="return confirm('Silmek istediğinizden emin misiniz?')" href="<?php echo PHP_D_URL; ?>&islem=delete_author&id=<?php echo $soz->wp_soz_author_id; ?>">Sil</a>
+                <a onclick="return confirm('Silmek istediğinizden emin misiniz?')" href="<?php echo PHP_D_URL; ?>&islem=delete_author&id=<?php echo $item->author_id; ?>">Sil</a>
             </td>
-            
+            <td><?php echo $item->author_name; ?></td>
         </tr>
-        <?php
-        }
-
-        ?>
+        <?php } ?>
     </table>
 
-    <?php            
-
-        sayfala(PHP_D_URL,$toplam_sayfa,$page,$page_limit,'&islem=author_list');
-        
+    <?php sayfala(PHP_D_URL, $count, $page, AHMETI_SOZ_LIMIT, '&islem=author_list');
 
 }else{
-    // Söz yok ise uyarı mesajı ver.
-    ?>
-    <p class="ahmeti_hata">Hiç yazar eklememişsiniz :(</p>
-    <?php
+    echo '<p class="ahmeti_hata">Hiç yazar eklememişsiniz :(</p>';
 }
-?>

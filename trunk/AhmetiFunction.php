ae2e4f48b42950d8f9c5cb00f512519bef108ce9
Ahmet Imamoglu <ahmet363@gmail.com>
2021-12-02 20:34:36 +0300

    AhmetiFunction güncellendi.

--- trunk/AhmetiFunction.php
+++ trunk/AhmetiFunction.php
@@ -1,6 +1,6 @@
 <?php
 
-function ahmeti_soz_kurulum()
+function ahmeti_wp_guzel_sozler_install()
 {
 	global $wpdb;
 
@@ -73,39 +73,17 @@ function ahmeti_soz_kurulum()
     }
 }
 
-function ahmeti_admin_head()
+function ahmeti_wp_guzel_sozler_admin_head()
 {
-    /* Wp Admin Head */
-    //wp_enqueue_script('jquery');
-    
-
-    //wp_register_script('AhmetiWpGuzelSozlerAdminJs', plugins_url().'/ahmeti-wp-guzel-sozler/js/kontrol.js', array( 'jquery' ));
-    //wp_enqueue_script('AhmetiWpGuzelSozlerAdminJs');
-    
-    wp_register_style( 'AhmetiWpGuzelSozlerAdminCss', plugins_url().'/ahmeti-wp-guzel-sozler/css/style.css');
-    wp_enqueue_style( 'AhmetiWpGuzelSozlerAdminCss' );
-    
-    
+    wp_register_style('AhmetiWpGuzelSozlerAdminCss', plugins_url().'/ahmeti-wp-guzel-sozler/css/style.css');
+    wp_enqueue_style('AhmetiWpGuzelSozlerAdminCss');
 }
 
-function ahmeti_wp_head()
-{
-    /* Wp Head */
-    wp_register_style( 'AhmetiWpGuzelSozlerAdminCss', plugins_url().'/ahmeti-wp-guzel-sozler/css/style.css',array(),'','screen' );
-    wp_enqueue_style( 'AhmetiWpGuzelSozlerAdminCss' );
-    
-}
-
-function ahmeti_admin_menu()
+function ahmeti_wp_guzel_sozler_admin_menu()
 {
     /* Admin Menü */
-    //add_action('admin_head','ahmeti_admin_head');
-    add_action( 'admin_init', 'ahmeti_admin_head' );
-    add_menu_page( 'Ahmeti Wp Güzel Sözler', 'Söz Arşivi', 'edit_pages', 'ahmeti_wp_guzel_sozler/index.php', 'ahmeti_index', plugins_url('ahmeti-wp-guzel-sozler/images/ahmeti-icon.png'), 6 );
-    
-    //add_action('admin_head','ahmeti_admin_head');
-    
-    
+    add_action('admin_init', 'ahmeti_wp_guzel_sozler_admin_head' );
+    add_menu_page('Ahmeti Wp Güzel Sözler', 'Söz Arşivi', 'edit_pages', 'ahmeti_wp_guzel_sozler/index.php', 'ahmeti_index', plugins_url('ahmeti-wp-guzel-sozler/images/ahmeti-icon.png'), 6);
 }
 
 function ahmeti_wp_guzel_sozler()
@@ -132,198 +110,58 @@ function ahmeti_wp_guzel_sozler()
     return $soz;
 }
 
-function ahmeti_wp_guzel_sozler_ayri()
+
+
+function ahmeti_wp_db()
 {
-    $ahmetiPre=AHMETI_WP_PREFIX;
-    $row=mysql_fetch_assoc(mysql_query("SELECT soz,aciklama,wp_soz_author_name,wp_soz_author_slug FROM {$ahmetiPre}soz_view ORDER BY RAND() LIMIT 0,1"));
-
-    $AhmetiSoz=htmlspecialchars(strip_tags($row['soz']), ENT_QUOTES, 'UTF-8');
-    $AhmetiYazar=htmlspecialchars(strip_tags($row['wp_soz_author_name']), ENT_QUOTES, 'UTF-8');
-    $AhmetiAciklama=htmlspecialchars(strip_tags($row['aciklama']), ENT_QUOTES, 'UTF-8');
-
-    $dondur=array('Soz'=>$AhmetiSoz,'Yazar'=>$AhmetiYazar,'Aciklama'=>$AhmetiAciklama);
-    
-    return $dondur;
+    global $wpdb;
+    return $wpdb;
 }
 
-/* Special Page Functions */
-function my_flush_rules(){
-    
-    $rules = get_option('rewrite_rules');
+function ahmeti_wp_guzel_sozler_sayfala($site_url, $top_sayfa, $page, $limit, $page_url)
+{
+    if( $limit >= $top_sayfa){ return ''; }
 
-    if ( ! isset( $rules['('.AHMETI_SOZ_LIST_SLUG.')/([a-zA-Z0-9-]+)$'] )  || ! isset( $rules['('.AHMETI_SOZ_LIST_SLUG.')/([a-zA-Z0-9-]+)/([a-zA-Z0-9-]+)$'] ) ) {
-        global $wp_rewrite;
-        $wp_rewrite->flush_rules();
-    }
-}
-    
-function my_insert_rewrite_rules( $rules ){
-    $newrules = array();
-    $newrules['('.AHMETI_SOZ_LIST_SLUG.')/([a-zA-Z0-9-]+)$'] = 'index.php?pagename=$matches[1]&soz_sahibi=$matches[2]'; 
-    $newrules['('.AHMETI_SOZ_LIST_SLUG.')/([a-zA-Z0-9-]+)/([a-zA-Z0-9-]+)$'] = 'index.php?pagename=$matches[1]&soz_sahibi=$matches[2]&soz_sayfa=$matches[3]';
+    echo '<div id="sayfala"><span class="say_sabit">Sayfalar</span>';
 
-    return $newrules + $rules; 
-}
+    $x = 5; // Aktif sayfadan önceki/sonraki sayfa gösterim sayisi
+    $lastP = ceil($top_sayfa / $limit);
 
-function my_insert_query_vars( $vars ){
-    array_push($vars, 'soz_sahibi');
-    array_push($vars, 'soz_sayfa');
-
-    return $vars;
-}
-/* Special Page Functions */
-
-if ( AHMETI_SOZ_LIST_SLUG != '' ){
-
-    add_action( 'wp_loaded','my_flush_rules' );
-    add_filter( 'rewrite_rules_array','my_insert_rewrite_rules' );
-    add_filter( 'query_vars','my_insert_query_vars' );
-}
-
-function ahmeti_wp_guzel_sozler_in_page(){
-    
-    if ( is_page() && AHMETI_SOZ_LIST_SLUG == get_query_var('pagename')  ){
-
-        // Ahmeti Wp Güzel Sözler Listesi İçin Url'den Gelen Talep Varsa...
-        $aa_soz_sayfa=get_query_var('soz_sayfa');
-        $sozSahibiSef=get_query_var('soz_sahibi');
-        
-        if (!empty($sozSahibiSef)){
-            ahmeti_wp_guzel_sozler_author_soz_list($sozSahibiSef);
-        }else{
-            ahmeti_wp_guzel_sozler_author_list();
-        }
-    }
-    
-}
-
-function ahmeti_wp_guzel_sozler_author_list(){
-    ?>
-    <h1 class="entry-title">
-        <a href="<?php echo site_url(); ?>/<?php echo AHMETI_SOZ_LIST_SLUG; ?>/" rel="bookmark">Harika Sözler</a>
-    </h1>
-
-    <?php
-    // INDEX
-    $ahmetiPre=AHMETI_WP_PREFIX;
-    $sql_ahmeti=mysql_query("SELECT wp_soz_author_name,wp_soz_author_slug FROM {$ahmetiPre}soz_author ORDER BY wp_soz_author_name ASC");
-    echo '<ul id="ahmeti_wp_author_list">';
-    while($row_ahmeti=mysql_fetch_object($sql_ahmeti)){
-
-        echo '<li><a href="'.site_url().'/'.AHMETI_SOZ_LIST_SLUG.'/'.$row_ahmeti->wp_soz_author_slug.'/">'.$row_ahmeti->wp_soz_author_name.'</a></li>';
-    }
-    echo '</ul>';    
-}
-
-function ahmeti_wp_guzel_sozler_author_soz_list($sozSahibiSef){
-    
-    // SOZ SAHIBI SAYFASI
-    $ahmetiPre=AHMETI_WP_PREFIX;
-    $sql_author=mysql_fetch_object(mysql_query("SELECT wp_soz_author_id,wp_soz_author_name FROM {$ahmetiPre}soz_author WHERE wp_soz_author_slug='$sozSahibiSef'"));
-
-    if (empty($sql_author->wp_soz_author_id)){
-        echo 'Bu kişi hakkında söz eklenmemiş.'; 
+    // sayfa 1'i yazdir
+    if ($page==1){
+        echo '<span class="say_aktif">1</span>';
     }else{
-    ?> 
-        <a style="float: right;font-weight: bold;margin-top: 7px;" href="<?php echo site_url(); ?>/<?php echo AHMETI_SOZ_LIST_SLUG; ?>/">&lt; Harika Sözler</a>
-        <h1 class="entry-title"><a href="<?php echo site_url(); ?>/<?php echo AHMETI_SOZ_LIST_SLUG; ?>/<?php echo $sozSahibiSef; ?>" rel="bookmark"><?php echo $sql_author->wp_soz_author_name; ?> Sözleri</a></h1>
-
-        <?php
-        $sql_soz_list=mysql_query("SELECT soz,aciklama,author_content FROM {$ahmetiPre}soz_view WHERE wp_soz_author_id='$sql_author->wp_soz_author_id' ");
-
-        $i=true;
-        while($row_soz_list=mysql_fetch_object($sql_soz_list)){
-
-            if ($i==true && !empty($row_soz_list->author_content)){
-
-                preg_match('/\[gallery.*ids=.(.*).\]/', $row_soz_list->author_content, $ids);
-
-                if (!empty($ids)){
-                    $array_ids=explode(',',$ids[1]);
-                    $i_foreach=0;
-                    foreach($array_ids as $thumb_id){
-                        echo '<dl class="gallery-item"><dt class="gallery-icon"><a href="'.wp_get_attachment_url( $thumb_id ).'" title="'.$sql_author->wp_soz_author_name.'" rel="lightbox['.$thumb_id.']"><img width="150" height="150" src="'.wp_get_attachment_thumb_url($thumb_id).'" class="attachment-thumbnail" alt="1"></a></dt></dl>';
-                        $i_foreach++;
-                        if ($i_foreach==3){$i_foreach=0; echo '<br style="clear: both">';}
-                    }
-
-                    if(@!empty($thumb_id)){
-                        echo '<br style="clear: both">';
-                    }
-
-
-                    //echo jqlb_apply_lightbox(do_shortcode($ids[0]));
-
-                    $row_soz_list->author_content=str_replace($ids[0],'',$row_soz_list->author_content);
-                    $row_soz_list->author_content=str_replace('<p></p>','',$row_soz_list->author_content);                    
-                }
-
-                echo $row_soz_list->author_content;
-
-            }
-
-            if ($i==true){
-                echo '<ul>';
-            }
-
-            $i=false;
-
-            echo '<li style="margin-bottom:5px">'.$row_soz_list->soz;
-            if (!empty($row_soz_list->aciklama)){
-                echo '<span style="font-size:11px;font-style:italic;color:gray">&nbsp;&nbsp;'.$row_soz_list->aciklama.'</span>';
-            }
-            echo'</li>';
-        }
-        echo '</ul>';
-
+        echo '<a class="say_a" href="'.$site_url.''.$page_url.'">1</a>';
     }
-    
-}
 
-function Sayfala($site_url,$top_sayfa,$page,$limit,$page_url)
-{
-	// Sayfalama Şeridimiz
+    // "..." veya direkt 2
+    if ( $page -$x > 2 ){
+        echo '<span class="say_b">...</span>';
+        $i = $page-$x;
+    }else{
+        $i = 2;
+    }
 
-	if ($top_sayfa > $limit) :
+    // +/- $x sayfalari yazdir
+    for ($i; $i <= $page + $x; $i++){
+        if ( $i == $page ){
+	        echo '<span class="say_aktif">'.$i.'</span>';
+        }else{
+	        echo '<a class="say_a" href="'.$site_url.''.$page_url.'&is_page='.$i.'">'.$i.'</a>';
+        }
+        if ( $i == $lastP ){
+	        break;
+        }
+    }
 
-		echo '<div id="sayfala"><span class="say_sabit">Sayfalar</span>';
-
-		$x = 5; // Aktif sayfadan önceki/sonraki sayfa gösterim sayisi
-		$lastP = ceil($top_sayfa / $limit);
-
-		// sayfa 1'i yazdir
-		if ($page==1){
-			echo '<span class="say_aktif">1</span>';
-		}else{
-			echo '<a class="say_a" href="'.$site_url.''.$page_url.'">1</a>';
-		}
-
-		// "..." veya direkt 2
-		if ($page-$x>2){
-			echo '<span class="say_b">...</span>';
-			$i = $page-$x;
-		}else{
-			$i = 2;
-		}
-		// +/- $x sayfalari yazdir
-		for ($i; $i<=$page+$x; $i++){
-			if ($i==$page)
-				echo '<span class="say_aktif">'.$i.'</span>';
-			else
-				echo '<a class="say_a" href="'.$site_url.''.$page_url.'&is_page='.$i.'">'.$i.'</a>';
-			if ($i==$lastP)
-				break;
-		}
-
-		// "..." veya son sayfa
-		if ($page+$x<$lastP-1){
-			echo '<span class="say_b">...</span>';
-			echo '<a class="say_a" href="'.$site_url.''.$page_url.'&is_page='.$lastP.'">'.$lastP.'</a>';
-		}elseif ($page+$x==$lastP-1){
-			echo '<a class="say_a" href="'.$site_url.''.$page_url.'&is_page='.$lastP.'">'.$lastP.'</a>';
-		}
-		echo '</div>';//#sayfala
-	endif;
+    // "..." veya son sayfa
+    if ( $page + $x < $lastP - 1 ){
+        echo '<span class="say_b">...</span>';
+        echo '<a class="say_a" href="'.$site_url.''.$page_url.'&is_page='.$lastP.'">'.$lastP.'</a>';
+    }elseif( $page + $x == $lastP - 1 ){
+        echo '<a class="say_a" href="'.$site_url.''.$page_url.'&is_page='.$lastP.'">'.$lastP.'</a>';
+    }
+    echo '</div>';
 }
 
 function ahmeti_wp_guzel_sozler_sef_link($link_yap)
@@ -356,23 +194,6 @@ function ahmeti_wp_guzel_sozler_sef_link($link_yap)
 	return $link_yap;
 }
 
-function DebugAhmetiGuzelSozler($data)
-{
-	ob_start();
-	print_r($data);
-	$out = ob_get_contents();
-	ob_end_clean();
-	echo '<pre font-size="10">';
-	echo htmlentities($out,ENT_QUOTES,'UTF-8');
-	echo '</pre>';
-}
-
-function ahmeti_wp_db()
-{
-    global $wpdb;
-    return $wpdb;
-}
-
 function ahmeti_wp_guzel_sozler_quotes($columns=[], $orderBy=['quote_id', 'DESC'], $limit = [0, 20])
 {
 	$columns = empty($columns) ? '*' : implode(',', $columns);
@@ -397,7 +218,6 @@ function ahmeti_wp_guzel_sozler_authors($columns=[], $orderBy=['author_id', 'DES
 		'LIMIT '.$limit[0].', '.$limit[1]
 		, ['']));
 }
-
 
 function ahmeti_wp_guzel_sozler_header()
 {
@@ -445,4 +265,110 @@ function ahmeti_wp_guzel_sozler_footer()
             '<a target="_blank" href="https://github.com/ahmeti/ahmeti-wp-guzel-sozler">Github</a>'.
 		'</p>'.
     '</div>';
+}
+
+
+// Front Functions
+
+function ahmeti_wp_guzel_sozler_shortcode()
+{
+	ahmeti_wp_guzel_sozler_random();
+
+	$urlSegments = array_values(array_filter(explode('/', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))));
+
+	if( count($urlSegments) == 2 && isset($urlSegments[0]) && isset($urlSegments[1]) ){
+		// Quotes
+		return ahmeti_wp_guzel_sozler_shortcode_quotes($urlSegments[0], $urlSegments[1]);
+	}elseif( count($urlSegments) == 1 && isset($urlSegments[0]) ){
+		// Authors
+		return ahmeti_wp_guzel_sozler_shortcode_authors($urlSegments[0]);
+	}
+}
+
+function ahmeti_wp_guzel_sozler_shortcode_authors($urlSegment)
+{
+	$authors = ahmeti_wp_guzel_sozler_authors(['author_id', 'author_name', 'author_slug'], ['author_name', 'ASC'], [0, 99999]);
+	echo '<ul id="ahmeti_wp_author_list">';
+	foreach($authors as $author){
+		echo '<li><a href="'.home_url().'/'.$urlSegment.'/'.$author->author_slug.'">'.$author->author_name.'</a></li>';
+	}
+	echo '</ul>';
+}
+
+function ahmeti_wp_guzel_sozler_shortcode_quotes($urlSegment, $authorSlug)
+{
+	global $wpdb;
+	$author = $wpdb->get_row($wpdb->prepare('SELECT * FROM '.AHMETI_WP_AUTHORS_TABLE.' WHERE author_slug = %s LIMIT 0,1', [$authorSlug]));
+
+	if ( empty($author->author_id) ){
+		echo 'Bu kişi hakkında söz eklenmemiş.';
+	}else{
+		?>
+        <a style="float: right;font-weight: bold;margin-top: 7px;" href="<?php echo home_url().'/'.$urlSegment; ?>">&lt; Harika Sözler</a>
+        <h1 class="entry-title"><a href="<?php  echo home_url().'/'.$urlSegment.'/'.$authorSlug ?>" rel="bookmark"><?php echo $author->author_name; ?> Sözleri</a></h1>
+
+		<?php
+
+		if ( ! empty ($author->author_content) ){
+
+			preg_match('/\[gallery.*ids=.(.*).\]/', $author->author_content, $ids);
+
+			if ( ! empty($ids) ){
+				$array_ids = explode(',', $ids[1]);
+				$i_foreach=0;
+				foreach($array_ids as $thumb_id){
+					echo '<dl class="gallery-item">'.
+					     '<dt class="gallery-icon">'.
+					     '<a href="'.wp_get_attachment_url( $thumb_id ).'" title="'.$author->author_name.'" rel="lightbox['.$thumb_id.']">'.
+					     '<img width="150" height="150" src="'.wp_get_attachment_thumb_url($thumb_id).'" class="attachment-thumbnail" alt="1">'.
+					     '</a>'.
+					     '</dt>'.
+					     '</dl>';
+
+					$i_foreach++;
+
+					if ( $i_foreach == 3 ){
+						$i_foreach=0;
+						echo '<br style="clear: both">';
+					}
+				}
+
+				if( ! empty($thumb_id) ){
+					echo '<br style="clear: both">';
+				}
+
+				$author->author_content = str_replace($ids[0], '', $author->author_content);
+				$author->author_content = str_replace('<p></p>', '', $author->author_content);
+			}
+
+			echo $author->author_content;
+		}
+
+		$quotes = $wpdb->get_results($wpdb->prepare('SELECT * FROM '.AHMETI_WP_QUOTES_TABLE.' WHERE quote_author_id = %d ORDER BY quote_id DESC', [$author->author_id]));
+
+		echo '<ul>';
+		foreach($quotes as $quote){
+			echo '<li style="margin-bottom:5px">'.$quote->quote;
+			if ( ! empty($quote->quote_desc) ){
+				echo '<span style="font-size:11px;font-style:italic;color:gray">&nbsp;&nbsp;'.$quote->quote_desc.'</span>';
+			}
+			echo'</li>';
+		}
+		echo '</ul>';
+	}
+}
+
+function ahmeti_wp_guzel_sozler_random()
+{
+	global $wpdb;
+    $columns = 'quote_id, author_id, author_name, author_slug, quote, quote_desc';
+	$quote = $wpdb->get_row($wpdb->prepare('SELECT '.$columns.' FROM '.AHMETI_WP_QUOTES_TABLE.' quote LEFT JOIN '.AHMETI_WP_AUTHORS_TABLE.' as author ON author.author_id = quote.quote_author_id ORDER BY RAND()', ['']));
+    return (object)[
+        'quote_id' => isset($quote->quote_id) ? $quote->quote_id : null,
+        'author_id' => isset($quote->author_id) ? $quote->author_id : null,
+        'author_name' => isset($quote->author_name) ? $quote->author_name : null,
+        'author_slug' => isset($quote->author_slug) ? $quote->author_slug : null,
+        'quote' => isset($quote->quote) ? $quote->quote : null,
+        'quote_desc' => isset($quote->quote_desc) ? $quote->quote_desc : null,
+    ];
 }

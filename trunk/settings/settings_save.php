e263092fa574ff8b3b9d4ed587d68353dec7b103
Ahmet Imamoglu <ahmet363@gmail.com>
2021-12-02 17:35:12 +0300

    settings/settings_save güncellendi.

--- trunk/settings/settings_save.php
+++ trunk/settings/settings_save.php
@@ -1,15 +1,19 @@
-<?php if(!defined('AHMETI_KONTROL')){ echo 'Bu dosyaya erşiminiz engellendi.'; exit(); } ?>
 <?php
-if ($_POST['sirala']=="DESC" || $_POST['sirala']=="ASC"){
-    if($_POST['sayfa_limit'] > 0 || $_POST['sayfa_limit'] < 101){
-        $deger=$_POST['sirala'].','.$_POST['sayfa_limit'].','.trim($_POST['sozListSlug']);
-        $guncelle=mysql_query("UPDATE wp_options SET option_value='$deger' WHERE option_name='ahmeti_soz_setting'");
 
-        if ($guncelle){
-            echo '<p class="ahmeti_ok">Ayarlar başarıyla güncellendi.</p>';
-        }else{
-            echo '<p class="ahmeti_hata">Ayarlar güncellenirken hata oluştu.</p>';
-        }
-    }
+if( ! defined('AHMETI_KONTROL') ){ echo 'Bu dosyaya erşiminiz engellendi.'; exit(); }
+
+if ( empty($_POST) ){
+	return '';
 }
-?>
+
+$orderByType = isset($_POST['sirala']) && in_array($_POST['sirala'], ['ASC', 'DESC']) ? $_POST['sirala'] : 'DESC';
+$pageLimit = isset($_POST['sayfa_limit']) && $_POST['sayfa_limit'] > 0 && $_POST['sayfa_limit'] < 101 ? $_POST['sayfa_limit'] : 20;
+$pageSlug = isset($_POST['page_slug']) && ! empty($_POST['page_slug']) ? $_POST['page_slug'] : 'harika-sozler';
+
+$status = update_option('ahmeti_soz_setting', $orderByType.','.$pageLimit.','.$pageSlug);
+
+if ( $status ){
+    echo '<p class="ahmeti_ok">Ayarlar başarıyla güncellendi.</p>';
+}else{
+	echo '<p class="ahmeti_hata">Ayarlar güncellenirken hata oluştu.</p>';
+}

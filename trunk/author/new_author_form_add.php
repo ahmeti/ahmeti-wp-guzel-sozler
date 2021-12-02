204fb16a5ab97cb118b47a4f0cad0dbb0107a42f
Ahmet Imamoglu <ahmet363@gmail.com>
2021-12-02 17:07:27 +0300

    author/new_author_form_add güncellendi.

--- trunk/author/new_author_form_add.php
+++ trunk/author/new_author_form_add.php
@@ -1,25 +1,29 @@
-<?php if(!defined('AHMETI_KONTROL')){ echo 'Bu dosyaya erşiminiz engellendi.'; exit(); } ?>
 <?php
-if (!empty($_POST)){
 
-    $kisi=mysql_real_escape_string(trim(stripslashes($_POST['kisi'])));
-    $author_content=mysql_real_escape_string(trim(stripslashes($_POST['author_content'])));
+if( ! defined('AHMETI_KONTROL') ){ echo 'Bu dosyaya erşiminiz engellendi.'; exit(); }
 
-    if(empty($kisi)){
+if ( empty($_POST) ){
+	return '';
+}
 
-        echo '<p class="ahmeti_hata">Boş alan bırakmayınız.</p>';
+$authorName = isset($_POST['kisi']) ? $_POST['kisi'] : '';
+$authorContent = isset($_POST['author_content']) ? $_POST['author_content'] : '';
 
+if( empty($authorName) ){
+
+    echo '<p class="ahmeti_hata">Boş alan bırakmayınız.</p>';
+
+}else{
+
+	$status = ahmeti_wp_db()->insert(AHMETI_WP_AUTHORS_TABLE, [
+		'author_name' => $authorName,
+		'author_slug' => ahmeti_wp_guzel_sozler_sef_link($authorName),
+		'author_content' => $authorContent,
+	]);
+
+    if ( $status ){
+        echo '<p class="ahmeti_ok">Yazar başarıyla eklendi.</p>';
     }else{
-
-        $slug_kisi=Sef_Link($kisi);
-        $ahmetiPre=AHMETI_WP_PREFIX;
-        $soz_yaz=mysql_query("insert into {$ahmetiPre}soz_author (wp_soz_author_name,wp_soz_author_slug,author_content) values ('$kisi','$slug_kisi','$author_content')");
-
-        if ($soz_yaz){
-            echo '<p class="ahmeti_ok">Kişi başarıyla eklendi.</p>';
-        }else{
-            echo '<p class="ahmeti_hata">Kişi eklenirken hata oluştu.</p>';
-        }                
+        echo '<p class="ahmeti_hata">Yazar eklenirken hata oluştu.</p>';
     }
 }
-?>

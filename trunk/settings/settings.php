18a6dfc7c992a96827a11b307e1193d63db57300
Ahmet Imamoglu <ahmet363@gmail.com>
2021-12-02 20:43:15 +0300

    settings/settings güncellendi.

--- trunk/settings/settings.php
+++ trunk/settings/settings.php
@@ -1,7 +1,7 @@
 <?php if( ! defined('AHMETI_KONTROL') ){ echo 'Bu dosyaya erşiminiz engellendi.'; exit(); } ?>
 
 <h2>Kullanım 1:</h2>
-<p>Sözü nerede göstermek istiyorsanız;<code>&lt;?php echo ahmeti_wp_guzel_sozler(); ?&gt;</code> eklemeniz yeterlidir.</p>
+<p>Yazar listesini hangi sayfada göstermek istiyorsanız; sayfanın, yazının içine <code>[ahmeti_wp_guzel_sozler]</code> eklemeniz yeterlidir.</p>
 
 <h2>Kullanım 2:</h2>
 <p>Kişiselleştirin. Değişkenleri parçalara bölerek kullanın...<br/>
@@ -9,33 +9,26 @@ Daha sonra sözü, yazarı ve söz açıklamasını istediğiniz yerde gösterin
 Sadece sözü, sadece yazarı veya sadece açıklamayı gösterebilirsiniz.</p>
 
 <h4>Örneğin;</h4>
-<p><code>&lt;?php $ahmetiSoz = ahmeti_wp_guzel_sozler_ayri(); ?&gt;</code> fonksiyonunu kullanarak dönen değerleri değişkene aktarın.</p>
-<p><code>&lt;?php echo $ahmetiSoz['Soz']; ?&gt;</code></p>
-<p><code>&lt;?php echo $ahmetiSoz['Yazar']; ?&gt;</code></p>
-<p><code>&lt;?php echo $ahmetiSoz['Aciklama']; ?&gt;</code></p>
-
-<h2>Kullanım 3:</h2>
-<p>Eklediğiniz sözlerin tamımını ziyaretçileriniz ile paylaşabilirsiniz.</p>
-
-<p>Bunun için ilk önce <code>Wp-Admin -> Sayfalar -> Yeni Sayfa Ekle</code> menüsünden bir sayfa oluşturmalısınız.</p>
-<p>Sayfanın <code>sef link</code> adını aşağıdaki Sayfa Benzersiz Adı kutusuna yazınız. Örneğin; harika-sozler</p>
-<p>Daha sonra kullandığınız temanın <code>page.php</code> dosyasının içine <code>&lt;?php the_content(); ?&gt;</code> fonksiyonundan önce <code>&lt;?php ahmeti_wp_guzel_sozler_in_page(); ?&gt;</code> fonksiyonu ekleyiniz.</p>
-
+<p><code>&lt;?php $soz = ahmeti_wp_guzel_sozler_random(); ?&gt;</code> fonksiyonunu kullanarak dönen değerleri değişkene aktarın.</p>
+<p><code>&lt;?php echo $soz->quote_id; ?&gt;</code></p>
+<p><code>&lt;?php echo $soz->author_id; ?&gt;</code></p>
+<p><code>&lt;?php echo $soz->author_name; ?&gt;</code></p>
+<p><code>&lt;?php echo $soz->author_slug; ?&gt;</code></p>
+<p><code>&lt;?php echo $soz->quote; ?&gt;</code></p>
+<p><code>&lt;?php echo $soz->quote_desc; ?&gt;</code></p>
 
 <div style="display: block;padding: 10px 0 10px 0">
     <form id="form_ayarlar" action="<?php echo PHP_D_URL; ?>&islem=ayar_kaydet" method="post">
 
-        <h3 style="margin-bottom: 1px;">Sayfa Benzersiz Adı</h3>
-        <input type="text" name="page_slug" value="<?php echo AHMETI_SOZ_LIST_SLUG; ?>" size="60" />
-        <br/><br/>
-
+        <?php /*
         <h3 style="margin-bottom: 1px;">Panel Söz Sıraması</h3>
         <select name="sirala">
             <option value="ASC" <?php if( AHMETI_SIRALAMA=="ASC" ){ echo 'selected="selected"';} ?>>Eskinden Yeniye Doğru</option>
             <option value="DESC" <?php if( AHMETI_SIRALAMA=="DESC" ){ echo 'selected="selected"';} ?>>Yeniden Eskiye Doğru</option>
         </select>
-        <br/><br/>
+        <br/><br/> */ ?>
 
+        <?php /*
         <h3 style="margin-bottom: 1px;">Panel Söz Sayfalama Limiti</h3>
         <select name="sayfa_limit">
             <option value="5" <?php if( AHMETI_SOZ_LIMIT=="5" ){ echo 'selected="selected"';} ?>>5</option>
@@ -48,10 +41,10 @@ Sadece sözü, sadece yazarı veya sadece açıklamayı gösterebilirsiniz.</p>
             <option value="50"<?php if( AHMETI_SOZ_LIMIT=="50" ){ echo 'selected="selected"';} ?>>50</option>
             <option value="100"<?php if( AHMETI_SOZ_LIMIT=="100" ){ echo 'selected="selected"';} ?>>100</option>
         </select>
-        <br/><br/>
+        <br/><br/> */ ?>
 
         <a style="margin-right:15px;" class="button" href="<?php echo admin_url(); ?>plugin-editor.php?file=ahmeti-wp-guzel-sozler%2Fcss%2Fstyle.css&plugin=ahmeti-wp-guzel-sozler%2Fstyle.css">CSS Sitili Düzenle</a>
-        <input type="submit" value="Ayarları Güncelle" class="button" />
+        <?php /* <input type="submit" value="Ayarları Güncelle" class="button" /> */ ?>
 
     </form>
 </div>

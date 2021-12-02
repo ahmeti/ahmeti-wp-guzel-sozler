d90a3b5b76744bb6ead6a1d637fde48b5aa9830f
Ahmet Imamoglu <ahmet363@gmail.com>
2021-12-02 16:23:57 +0300

    soz/update_soz_form güncellendi.

--- trunk/soz/update_soz_form.php
+++ trunk/soz/update_soz_form.php
@@ -1,35 +1,33 @@
 <?php if(!defined('AHMETI_KONTROL')){ echo 'Bu dosyaya erşiminiz engellendi.'; exit(); } ?>
-<h2>Söz Düzenle</h2>
-<?php
-// Söz Güncelleme Sayfası
 
-$id=(int)$_GET['id'];
-$ahmetiPre=AHMETI_WP_PREFIX;
-$row=mysql_fetch_object(mysql_query("SELECT * FROM {$ahmetiPre}soz WHERE soz_id=$id"));
+<h2>Söz Düzenle</h2>
+
+<?php
+global $wpdb;
+$id = isset($_GET['id']) ? (int)$_GET['id'] : 0;
+$quote = $wpdb->get_row($wpdb->prepare('SELECT * FROM '.AHMETI_WP_QUOTES_TABLE.' WHERE quote_id = %d', [$id]));
 
 ?>
 <div style="display: block;padding: 0 0 10px 0">
     <form id="form_gonder" action="<?php echo PHP_D_URL; ?>&islem=guncelle_ok" method="post"><!-- onsubmit="return false;" -->
         <h3 style="margin-bottom: 1px;">Söz</h3>
-        <textarea id="soz" name="soz" style="width: 400px;height: 200px;border: 1px solid #ddd;padding: 5px"><?php echo $row->soz; ?></textarea>
+        <textarea id="soz" name="soz" style="width: 400px;height: 200px;border: 1px solid #ddd;padding: 5px"><?php echo $quote->quote; ?></textarea>
         <br/><br/>
         <h3 style="margin-bottom: 1px;">Açıklama</h3>
-        <textarea name="aciklama" style="width: 400px;height: 200px;border: 1px solid #ddd;padding: 5px"><?php echo $row->aciklama; ?></textarea>
+        <textarea name="aciklama" style="width: 400px;height: 200px;border: 1px solid #ddd;padding: 5px"><?php echo $quote->quote_desc; ?></textarea>
         <br/><br/>
         <h3 style="margin-bottom: 1px;">Yazar</h3>
         <select name="author_id">
             <option value="">Seçiniz...</option>
         <?php
-            $sql_author=mysql_query("SELECT * FROM {$ahmetiPre}soz_author ORDER BY wp_soz_author_name ASC");
-            while($row_author=mysql_fetch_object($sql_author)){
-            ?>
-            <option value="<?php echo $row_author->wp_soz_author_id; ?>" <?php if ($row_author->wp_soz_author_id==$row->soz_author_id) : echo 'selected="selected"'; endif; ?>><?php echo $row_author->wp_soz_author_name; ?></option>
-            <?php
+            $authors = ahmeti_wp_guzel_sozler_authors([], ['author_id', 'DESC'], [0, 99999]);
+            foreach($authors as $author){
+                echo '<option value="'.$author->author_id.'" '.($author->author_id == $quote->quote_author_id ? 'selected' : '').'>'.$author->author_name.'</option>';
             }
         ?>
         </select>
         <br/><br/>
         <input type="submit" value="Sözü Güncelle" class="button" id="gonder_button"/>
-        <input type="hidden" name="id" value="<?php echo $row->soz_id; ?>" />
+        <input type="hidden" name="id" value="<?php echo $id; ?>" />
     </form>
 </div>

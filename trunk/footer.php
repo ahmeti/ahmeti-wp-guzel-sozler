7d017c703625bc2258ce54d5d12bef2b5bcbe914
Ahmet Imamoglu <ahmet363@gmail.com>
2021-12-02 15:55:30 +0300

    footer silindi.

--- trunk/footer.php
+++
@@ -1,5 +0,0 @@
-<?php if(!defined('AHMETI_KONTROL')){ echo 'Bu dosyaya erşiminiz engellendi.'; exit(); } ?>
-<?php
-    echo '<br/><br/><p style="margin:30px 0 0 0 ;display:inline;font:oblique 11px/1em Georgia,serif">Geliştirici: <a target="_blank" href="http://ahmeti.net/">Ahmet İmamoğlu</a> | <p style="margin:30px 0 0 0 ;display:inline;font:oblique 11px/1em Georgia,serif"><a target="_blank" href="http://ahmeti.net/ahmeti-wp-guzel-sozler-1-0/">Eklenti Sayfası</a></p>';
-    echo '</div>'; //wrap_div
-?>

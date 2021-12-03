ddfff4147e0e48f62eadeb5bef42fd7bc461825f
Ahmet Imamoglu <ahmet363@gmail.com>
2021-12-03 10:46:48 +0300

    settings/settings güncellendi.

--- trunk/settings/settings.php
+++ trunk/settings/settings.php
@@ -20,31 +20,12 @@ Sadece sözü, sadece yazarı veya sadece açıklamayı gösterebilirsiniz.</p>
 <div style="display: block;padding: 10px 0 10px 0">
     <form id="form_ayarlar" action="<?php echo PHP_D_URL; ?>&islem=ayar_kaydet" method="post">
 
-        <?php /*
-        <h3 style="margin-bottom: 1px;">Panel Söz Sıraması</h3>
-        <select name="sirala">
-            <option value="ASC" <?php if( AHMETI_SIRALAMA=="ASC" ){ echo 'selected="selected"';} ?>>Eskinden Yeniye Doğru</option>
-            <option value="DESC" <?php if( AHMETI_SIRALAMA=="DESC" ){ echo 'selected="selected"';} ?>>Yeniden Eskiye Doğru</option>
-        </select>
-        <br/><br/> */ ?>
-
-        <?php /*
-        <h3 style="margin-bottom: 1px;">Panel Söz Sayfalama Limiti</h3>
-        <select name="sayfa_limit">
-            <option value="5" <?php if( AHMETI_SOZ_LIMIT=="5" ){ echo 'selected="selected"';} ?>>5</option>
-            <option value="10"<?php if( AHMETI_SOZ_LIMIT=="10" ){ echo 'selected="selected"';} ?>>10</option>
-            <option value="15"<?php if( AHMETI_SOZ_LIMIT=="15" ){ echo 'selected="selected"';} ?>>15</option>
-            <option value="20"<?php if( AHMETI_SOZ_LIMIT=="20" ){ echo 'selected="selected"';} ?>>20</option>
-            <option value="25"<?php if( AHMETI_SOZ_LIMIT=="25" ){ echo 'selected="selected"';} ?>>25</option>
-            <option value="30"<?php if( AHMETI_SOZ_LIMIT=="30" ){ echo 'selected="selected"';} ?>>30</option>
-            <option value="40"<?php if( AHMETI_SOZ_LIMIT=="40" ){ echo 'selected="selected"';} ?>>40</option>
-            <option value="50"<?php if( AHMETI_SOZ_LIMIT=="50" ){ echo 'selected="selected"';} ?>>50</option>
-            <option value="100"<?php if( AHMETI_SOZ_LIMIT=="100" ){ echo 'selected="selected"';} ?>>100</option>
-        </select>
-        <br/><br/> */ ?>
+        <h3 style="margin-bottom: 1px;">Sayfa Benzersiz Adı</h3>
+        <input type="text" name="page_slug" value="<?php echo ahmeti_wp_guzel_sozler_get_option('author_page_name'); ?>" size="60" />
+        <br/><br/>
 
         <a style="margin-right:15px;" class="button" href="<?php echo admin_url(); ?>plugin-editor.php?file=ahmeti-wp-guzel-sozler%2Fcss%2Fstyle.css&plugin=ahmeti-wp-guzel-sozler%2Fstyle.css">CSS Sitili Düzenle</a>
-        <?php /* <input type="submit" value="Ayarları Güncelle" class="button" /> */ ?>
+        <input type="submit" value="Ayarları Güncelle" class="button" />
 
     </form>
 </div>
